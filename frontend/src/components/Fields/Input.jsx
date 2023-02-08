30cf06c9d747e9781c81c3f7344950c0cbcadd81
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-02-09 06:30:05 +0700

    Create textarea component.

--- frontend/src/components/Fields/Input.jsx
+++ frontend/src/components/Fields/Input.jsx
@@ -9,18 +9,30 @@ const Input = ({
     placeholder,
     className,
     error,
+    rows,
 }) => {
     return (
         <div className={`${className}`}>
             <label className="ml-1">{label}</label>
-            <input
-                type={type ?? "text"}
-                className="field"
-                name={name}
-                value={value}
-                onChange={onChange}
-                placeholder={placeholder}
-            />
+            {type === "textarea" ? (
+                <textarea
+                    className="field"
+                    name={name}
+                    value={value}
+                    onChange={onChange}
+                    placeholder={placeholder}
+                    rows={rows ?? "3"}
+                ></textarea>
+            ) : (
+                <input
+                    type={type ?? "text"}
+                    className="field"
+                    name={name}
+                    value={value}
+                    onChange={onChange}
+                    placeholder={placeholder}
+                />
+            )}
             {error && (
                 <p className="text-sm font-medium text-red-500 ml-1 mt-2 -mb-2">
                     {error}
