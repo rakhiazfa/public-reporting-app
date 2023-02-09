a322f8cfcaf8d525df384b73d47ac4c03f7b2c4e
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-02-09 18:26:31 +0700

    Update user factory.

--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -2,7 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\Role;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /**
@@ -21,7 +23,8 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make('q1w2e3r4t5y6'),
+            'role_id' => Role::inRandomOrder()->first()->id,
             'remember_token' => Str::random(10),
         ];
     }
