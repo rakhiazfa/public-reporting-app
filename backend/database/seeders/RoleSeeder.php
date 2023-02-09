dd9d119d6d35d98b149bcca67e34dace890a12e7
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-02-09 15:28:40 +0700

    Create role seeder.

---
+++ backend/database/seeders/RoleSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+
+        Role::create(['name' => 'admin']);
+
+        Role::create(['name' => 'agency']);
+
+        Role::create(['name' => 'society']);
+    }
+}
