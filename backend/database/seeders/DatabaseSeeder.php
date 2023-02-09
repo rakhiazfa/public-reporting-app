f8effd84281c4c7e83489919af97035395b8337d
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-02-09 15:32:37 +0700

    Create user seeder.

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             RoleSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
---
+++ backend/database/seeders/UserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+
+        $user = new User([
+            'name' => 'Admin',
+            'email' => 'lapmas@example.co.id',
+            'username' => 'lapmas',
+            'password' => Hash::make('q1w2e3r4t5y6'),
+        ]);
+
+        $user->role()->associate(1);
+
+        $user->save();
+    }
+}
