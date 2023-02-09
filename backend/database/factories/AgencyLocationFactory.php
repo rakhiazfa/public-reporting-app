62ef6b5ccb045bb5b6285dc7f205b34de65c14d6
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-02-09 18:24:12 +0700

    Create agency location seeder.

---
+++ backend/database/factories/AgencyLocationFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\AgencyLocation>
+ */
+class AgencyLocationFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'country' => 'Indonesia',
+            'province' => fake()->country(),
+            'city' => fake()->city(),
+            'postal_code' => fake()->postcode(),
+            'address' => fake()->streetAddress(),
+        ];
+    }
+}
