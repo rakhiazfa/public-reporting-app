7e7b8491cd93ff91f17f1c5e6cfc55ae682b6464
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-02-09 15:45:45 +0700

    Auto bind repository and service.

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->bindRepositories();
+
+        $this->bindServices();
     }
 
     /**
@@ -24,5 +27,59 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+    }
+
+    /**
+     * Binds the repository with its interface.
+     * 
+     * @return void
+     */
+    protected function bindRepositories()
+    {
+        $path = app_path('Repositories');
+        $directories = file_exists($path) ? File::directories($path) : [];
+
+        foreach ($directories as $directory) {
+
+            $files = file_exists($directory) ? File::files($directory) : [];
+
+            $classes = [];
+
+            foreach ($files as $file) {
+
+                $directory = basename($directory);
+
+                $classes[] = 'App\Repositories\\' . $directory . '\\' . $file->getFilenameWithoutExtension();
+            }
+
+            (count($classes) > 0) && $this->app->bind($classes[0], $classes[1]);
+        }
+    }
+
+    /**
+     * Binds the service with its interface.
+     * 
+     * @return void
+     */
+    protected function bindServices()
+    {
+        $path = app_path('Services');
+        $directories = (file_exists($path)) ? File::directories($path) : [];
+
+        foreach ($directories as $directory) {
+
+            $files = (file_exists($directory)) ? File::files($directory) : [];
+
+            $classes = [];
+
+            foreach ($files as $file) {
+
+                $directory = basename($directory);
+
+                $classes[] = 'App\Services\\' . $directory . '\\' . $file->getFilenameWithoutExtension();;
+            }
+
+            (count($classes) > 0) && $this->app->bind($classes[0], $classes[1]);
+        }
     }
 }
