726bde9b34eb1177bc75c578a3394750b666acc7
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-02-09 15:29:49 +0700

    Add username attribute.

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
 
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('username')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
