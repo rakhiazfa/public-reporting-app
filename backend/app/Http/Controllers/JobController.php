1b2fe309bbeb7840f2dea37f64507ac746cd3781
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-02-09 19:12:42 +0700

    Create job controller.

---
+++ backend/app/Http/Controllers/JobController.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Job\StoreJobRequest;
+use App\Http\Requests\Job\UpdateJobRequest;
+use App\Models\Job;
+use App\Repositories\Job\JobRepository;
+use Illuminate\Http\Request;
+
+class JobController extends Controller
+{
+    /**
+     * @var JobRepository
+     */
+    protected JobRepository $jobRepository;
+
+    /**
+     * @param JobRepository $jobRepository
+     */
+    public function __construct(JobRepository $jobRepository)
+    {
+        $this->jobRepository = $jobRepository;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        try {
+
+            $jobs = $this->jobRepository->orderByIdDesc();
+
+            // 
+        } catch (\Exception $exception) {
+
+            return response()->json([
+                'success' => false,
+                'code' => $exception->getCode(),
+                'message' => $exception->getMessage(),
+            ], $exception->getCode());
+        }
+
+        return response()->json([
+            'success' => true,
+            'code' => 200,
+            'jobs' => $jobs,
+        ], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreJobRequest $request)
+    {
+        try {
+
+            $job = $this->jobRepository->create($request->only(['name']));
+
+            // 
+        } catch (\Exception $exception) {
+
+            return response()->json([
+                'success' => false,
+                'code' => $exception->getCode(),
+                'message' => $exception->getMessage(),
+            ], $exception->getCode());
+        }
+
+        return response()->json([
+            'success' => true,
+            'code' => 201,
+            'message' => 'Successfully created a new job.',
+            'job' => $job,
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  Job $job
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Job $job)
+    {
+        return response()->json([
+            'success' => true,
+            'code' => 200,
+            'job' => $job,
+        ], 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  Job $job
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateJobRequest $request, Job $job)
+    {
+        $job->update($request->all());
+
+        return response()->json([
+            'success' => true,
+            'code' => 200,
+            'message' => 'Successfully updated job.',
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  Job $job
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Job $job)
+    {
+        $job->delete();
+
+        return response()->json([
+            'success' => true,
+            'code' => 200,
+            'message' => 'Successfully deleted job.',
+        ], 200);
+    }
+}
