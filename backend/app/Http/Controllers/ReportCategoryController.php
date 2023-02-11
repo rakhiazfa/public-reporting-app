06396274478834e258323afb2cb4e9f3774817b4
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-02-11 15:46:19 +0700

    Remove useless method.

--- backend/app/Http/Controllers/ReportCategoryController.php
+++ backend/app/Http/Controllers/ReportCategoryController.php
@@ -56,16 +56,6 @@ class ReportCategoryController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -83,17 +73,6 @@ class ReportCategoryController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
     {
         //
     }
