ae5b381c2ff6373cf4e270cbc1865c9f2c0ad27d
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-02-11 12:08:26 +0700

    Create a relationship between report category and report subcategory.

--- backend/app/Models/ReportCategory.php
+++ backend/app/Models/ReportCategory.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class ReportCategory extends Model
 {
     use HasFactory;
+
+    /**
+     * @return HasMany
+     */
+    public function reportSubcategories()
+    {
+        return $this->hasMany(ReportSubcategory::class, 'report_category_id', 'id');
+    }
 }
--- backend/app/Models/ReportSubcategory.php
+++ backend/app/Models/ReportSubcategory.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ReportSubcategory extends Model
 {
     use HasFactory;
+
+    /**
+     * @return BelongsTo
+     */
+    public function reportCategory()
+    {
+        return $this->belongsTo(ReportCategory::class, 'report_category_id', 'id');
+    }
 }
