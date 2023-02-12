a1a5cf5e2ecfa515b69032eabcca95730f5c2bc3
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-02-12 17:10:33 +0700

    Create a method for create a new report subcategory.

--- backend/app/Services/ReportCategory/ReportCategoryService.php
+++ backend/app/Services/ReportCategory/ReportCategoryService.php
@@ -3,8 +3,18 @@
 namespace App\Services\ReportCategory;
 
 use App\Foundation\Service\ServiceInterface;
+use App\Models\ReportCategory;
+use Illuminate\Database\Eloquent\Model;
 
 interface ReportCategoryService extends ServiceInterface
 {
-    // 
+    /**
+     * Create a new subcategory.
+     * 
+     * @param ReportCategory $reportCategory
+     * @param array $attributes
+     * 
+     * @return Model
+     */
+    public function createSubcategory(ReportCategory $reportCategory, array $attributes = []): Model;
 }
--- backend/app/Services/ReportCategory/ReportCategoryServiceImplementation.php
+++ backend/app/Services/ReportCategory/ReportCategoryServiceImplementation.php
@@ -3,15 +3,45 @@
 namespace App\Services\ReportCategory;
 
 use App\Foundation\Service\ServiceImplementation;
+use App\Models\ReportCategory;
 use App\Repositories\ReportCategory\ReportCategoryRepository;
+use App\Repositories\ReportSubcategory\ReportSubcategoryRepository;
+use Illuminate\Database\Eloquent\Model;
 
 class ReportCategoryServiceImplementation extends ServiceImplementation implements ReportCategoryService
 {
     /**
+     * @var ReportSubcategoryRepository
+     */
+    protected ReportSubcategoryRepository $reportSubcategoryRepository;
+
+    /**
      * @param ReportCategoryRepository $repository
      */
-    public function __construct(ReportCategoryRepository $repository)
-    {
+    public function __construct(
+        ReportCategoryRepository $repository,
+        ReportSubcategoryRepository $reportSubcategoryRepository
+    ) {
         $this->repository = $repository;
+        $this->reportSubcategoryRepository = $reportSubcategoryRepository;
+    }
+
+    /**
+     * Create a new report subcategory.
+     * 
+     * @param ReportCategory $reportCategory
+     * @param array $attributes
+     * 
+     * @return Model
+     */
+    public function createSubcategory(ReportCategory $reportCategory, array $attributes = []): Model
+    {
+        $subcategory = $this->reportSubcategoryRepository->new($attributes);
+
+        $subcategory->reportCategory()->associate($reportCategory);
+
+        $subcategory->save();
+
+        return $subcategory;
     }
 }
