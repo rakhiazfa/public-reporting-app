6295479b0a1015f9dade91f1a9c8275a261634a6
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-02-12 16:55:22 +0700

    Create exception response.

---
+++ backend/app/Exceptions/ExceptionResponse.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+use Illuminate\Http\Response;
+
+class ExceptionResponse
+{
+    /**
+     * @var Exception
+     */
+    protected Exception $exception;
+
+    /**
+     * Create a new ExceptionResponse instance.
+     * 
+     * @param Exception $exception
+     */
+    public function __construct(Exception $exception)
+    {
+        $this->exception = $exception;
+    }
+
+    /**
+     * Conver exception to json.
+     * 
+     * @return Response
+     */
+    public function json()
+    {
+        return response()->json([
+            'success' => false,
+            'code' => $this->exception->getCode(),
+            'message' => $this->exception->getMessage(),
+        ], $this->exception->getCode());
+    }
+}
