30014bc8f44cb7569cd871e6d414feacca4c2d28
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-02-14 02:06:03 +0700

    Add name attribute.

--- backend/database/migrations/2023_02_09_111145_create_agencies_table.php
+++ backend/database/migrations/2023_02_09_111145_create_agencies_table.php
@@ -16,6 +16,8 @@ return new class extends Migration
         Schema::create('agencies', function (Blueprint $table) {
             $table->id();
 
+            $table->string('name')->unique();
+
             $table->foreignId('user_id')->unique()
                 ->constrained('users')->cascadeOnDelete();
 
