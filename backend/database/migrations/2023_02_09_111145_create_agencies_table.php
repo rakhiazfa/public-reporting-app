2cf88e496d89baf26caa7bcab96ba93aab9c529c
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-02-13 12:34:30 +0700

    Make the user_id field unique.

--- backend/database/migrations/2023_02_09_111145_create_agencies_table.php
+++ backend/database/migrations/2023_02_09_111145_create_agencies_table.php
@@ -18,7 +18,8 @@ return new class extends Migration
 
             $table->string('name')->unique();
 
-            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('user_id')->unique()
+                ->constrained('users')->cascadeOnDelete();
 
             $table->timestamps();
         });
