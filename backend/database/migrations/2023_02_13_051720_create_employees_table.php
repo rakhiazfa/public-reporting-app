ca9997c499c357ed54efee8cce09c372080229c7
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-02-13 12:41:14 +0700

    Create employee table.

---
+++ backend/database/migrations/2023_02_13_051720_create_employees_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('nip')->unique();
+
+            $table->foreignId('agency_id')->constrained('agencies')->cascadeOnDelete();
+
+            $table->foreignId('user_id')->unique()
+                ->constrained('users')->cascadeOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+};
