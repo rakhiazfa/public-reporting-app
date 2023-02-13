6bb2bafca0363a20742576455d225904eba83aff
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-02-13 22:53:10 +0700

    [FIX] class attribute - warning.

--- frontend/src/components/Fields/File.jsx
+++ frontend/src/components/Fields/File.jsx
@@ -13,7 +13,7 @@ const File = ({
         <div className={`${className}`}>
             <label className="ml-1">{label}</label>
             <input
-                class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50  focus:outline-none"
+                className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50  focus:outline-none"
                 aria-describedby="file_input_help"
                 id="file_input"
                 type="file"
@@ -22,7 +22,7 @@ const File = ({
                 onChange={onChange}
                 placeholder={placeholder}
             />
-            <p class="mt-1 text-sm text-gray-500" id="file_input_help">
+            <p className="mt-1 text-sm text-gray-500" id="file_input_help">
                 SVG, PNG, JPG or GIF (MAX. 800x400px).
             </p>
             {error && (
