7ec8dcc4f4335db2a7c5d3e87cc04103f5664254
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-02-14 02:10:56 +0700

    Create store agency request.

---
+++ backend/app/Http/Requests/Agency/StoreAgencyRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests\Agency;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class StoreAgencyRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return Auth::check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['required', 'unique:agencies'],
+            'country' => ['required'],
+            'province' => ['required'],
+            'city' => ['required'],
+            'postal_code' => ['required'],
+            'address' => ['required'],
+        ];
+    }
+}
