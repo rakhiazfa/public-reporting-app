9fa2e99b8a26fb7adb9eb3450fb7bffc8cd9bdfc
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-02-15 23:18:40 +0700

    Update society request.

--- backend/app/Http/Requests/Society/UpdateSocietyRequest.php
+++ backend/app/Http/Requests/Society/UpdateSocietyRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests\Society;
 
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
 
 class UpdateSocietyRequest extends FormRequest
 {
@@ -24,8 +25,23 @@ class UpdateSocietyRequest extends FormRequest
      */
     public function rules()
     {
+        $society = $this->route('society');
+
         return [
-            //
+            'nik' => ['required', 'unique:societies,nik,' . $society->id],
+            'name' => ['required'],
+            'date_of_birth' => ['required', 'date'],
+            'gender' => ['required', Rule::in(['Pria', 'Wanita'])],
+            'phone' => ['required'],
+            'job_id' => ['required'],
+            'email' => ['required', 'unique:users,email,' . $society->user->id],
+            'username' => ['required', 'unique:users,username,' . $society->user->id, 'without_spaces'],
+            'password' => ['required', 'min:8', 'confirmed'],
+            'country' => ['required'],
+            'province' => ['required'],
+            'city' => ['required'],
+            'postal_code' => ['required'],
+            'address' => ['required'],
         ];
     }
 }
