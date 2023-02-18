b0e88d34a07a82280e8d31135015bcf134233af6
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-02-18 22:16:53 +0700

    Update profile page.

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -52,27 +52,6 @@ const Profile = () => {
 
     //
 
-    const [province, setProvince] = useState({
-        value: user?.society?.location?.province,
-        label: user?.society?.location?.province,
-    });
-    const [city, setCity] = useState({
-        value: user?.society?.location?.city,
-        label: user?.society?.location?.city,
-    });
-    const [subDistrict, setSubDistrict] = useState({
-        value: user?.society?.location?.sub_district,
-        label: user?.society?.location?.sub_district,
-    });
-    const [urbanVillage, setUrbanVillage] = useState({
-        value: user?.society?.location?.urban_village,
-        label: user?.society?.location?.urban_village,
-    });
-    const [postalCode, setPostalCode] = useState(
-        user?.society?.location?.postal_code
-    );
-    const [address, setAddress] = useState(user?.society?.location?.address);
-
     const handleDateOfBirthChange = (newValue) => {
         setDateOfBirth(newValue);
     };
@@ -95,107 +74,9 @@ const Profile = () => {
             name,
             email,
             username,
-            // Location
-            province: province?.value,
-            city: city?.value,
-            sub_district: subDistrict?.value,
-            urban_village: urbanVillage?.value,
-            postal_code: postalCode,
-            address,
         };
 
         console.log(payload);
-    };
-
-    // Fetch all provinces.
-    const fetchAllProvinces = async () => {
-        try {
-            const { data } = await axios.get(
-                "http://dev.farizdotid.com/api/daerahindonesia/provinsi"
-            );
-
-            const provinsi = data?.provinsi?.reduce((prev, next) => {
-                prev.push({
-                    value: next?.nama,
-                    label: next?.nama,
-                });
-
-                return prev;
-            }, []);
-
-            setProvinces(provinsi);
-        } catch (error) {
-            console.log(error);
-        }
-    };
-
-    // Fetch all cities.
-    const fetchAllCities = async (provinceId) => {
-        try {
-            const { data } = await axios.get(
-                `http://dev.farizdotid.com/api/daerahindonesia/kota?id_provinsi=${provinceId}`
-            );
-
-            const kota_kabupaten = data?.kota_kabupaten?.reduce(
-                (prev, next) => {
-                    prev.push({
-                        value: next?.nama,
-                        label: next?.nama,
-                    });
-
-                    return prev;
-                },
-                []
-            );
-
-            setCities(kota_kabupaten);
-        } catch (error) {
-            console.log(error);
-        }
-    };
-
-    // Fetch all sub districts.
-    const fetchAllSubDistrict = async (cityId) => {
-        try {
-            const { data } = await axios.get(
-                `https://dev.farizdotid.com/api/daerahindonesia/kecamatan?id_kota=${cityId}`
-            );
-
-            const kecamatan = data?.kecamatan?.reduce((prev, next) => {
-                prev.push({
-                    value: next?.nama,
-                    label: next?.nama,
-                });
-
-                return prev;
-            }, []);
-
-            setSubDistricts(kecamatan);
-        } catch (error) {
-            console.log(error);
-        }
-    };
-
-    // Fetch all urban villages.
-    const fetchAllUrbanVillages = async (subDistrictId) => {
-        try {
-            const { data } = await axios.get(
-                `https://dev.farizdotid.com/api/daerahindonesia/kelurahan?id_kecamatan=${subDistrictId}`
-            );
-
-            const kelurahan = data?.kelurahan?.reduce((prev, next) => {
-                prev.push({
-                    value: next?.nama,
-                    label: next?.nama,
-                });
-
-                return prev;
-            }, []);
-
-            seturbanVillages(kelurahan);
-        } catch (error) {
-            console.log(error);
-        }
     };
 
     // Fetch all jobs.
@@ -210,9 +91,9 @@ const Profile = () => {
     };
 
     useEffect(() => {
-        fetchAllProvinces();
-        fetchAllJobs();
-        return () => {};
+        return () => {
+            fetchAllJobs();
+        };
     }, []);
 
     return (
@@ -327,110 +208,6 @@ const Profile = () => {
                                             error={errors?.username}
                                         />
                                     </div>
-                                </div>
-                            </div>
-                            <div className="mt-7">
-                                <h1 className="text-xl md:text-2xl text-gray-500 font-semibold mb-5">
-                                    Alamat
-                                </h1>
-                                <div className="grid gap-7 mb-7">
-                                    <div className="grid grid-cols-1 md:grid-cols-2 gap-7">
-                                        <Select
-                                            label="Provinsi"
-                                            options={provinces}
-                                            onChange={(type) => {
-                                                fetchAllCities(type?.value);
-
-                                                setProvince({
-                                                    id: type?.value,
-                                                    name: type?.label,
-                                                });
-                                            }}
-                                            value={province}
-                                            placeholder="Pilih provinsi anda . . ."
-                                            error={errors?.province}
-                                        />
-                                        <Select
-                                            label="Kota"
-                                            options={cities}
-                                            onChange={(type) => {
-                                                fetchAllSubDistrict(
-                                                    type?.value
-                                                );
-
-                                                setCity({
-                                                    id: type?.value,
-                                                    name: type?.label,
-                                                });
-                                            }}
-                                            value={city}
-                                            placeholder="Pilih kota anda . . . "
-                                            disabled={!province?.value}
-                                            error={errors?.city}
-                                        />
-                                        <Select
-                                            label="Kecamatan"
-                                            options={subDistricts}
-                                            onChange={(type) => {
-                                                fetchAllUrbanVillages(
-                                                    type?.value
-                                                );
-
-                                                setSubDistrict({
-                                                    id: type?.value,
-                                                    name: type?.label,
-                                                });
-                                            }}
-                                            value={subDistrict}
-                                            placeholder="Pilih kecamatan anda . . . "
-                                            disabled={!city?.value}
-                                            error={errors?.sub_district}
-                                        />
-                                        <Select
-                                            label="Kelurahan"
-                                            options={urbanVillages}
-                                            onChange={(type) =>
-                                                setUrbanVillage({
-                                                    id: type?.value,
-                                                    name: type?.label,
-                                                })
-                                            }
-                                            value={urbanVillage}
-                                            placeholder="Pilih kelurahan anda . . . "
-                                            disabled={!subDistrict?.value}
-                                            error={errors?.urban_village}
-                                        />
-                                        <Input
-                                            type="text"
-                                            label="Kode Pos"
-                                            placeholder="Masukan kode pos anda . . ."
-                                            className="md:col-span-2"
-                                            onChange={(e) =>
-                                                setPostalCode(e.target.value)
-                                            }
-                                            value={postalCode}
-                                            error={errors?.postal_code}
-                                        />
-                                    </div>
-                                    <Input
-                                        type="textarea"
-                                        label="Alamat"
-                                        placeholder="Masukan alamat anda . . ."
-                                        onChange={(e) =>
-                                            setAddress(e.target.value)
-                                        }
-                                        value={address}
-                                        error={errors?.address}
-                                    />
-                                </div>
-                                <div className="flex justify-end col-span-2">
-                                    <button
-                                        type="submit"
-                                        className="button bg-blue-600 hover:bg-blue-700 text-white rounded-full"
-                                        disabled={loading}
-                                    >
-                                        Simpan
-                                    </button>
                                 </div>
                             </div>
                         </form>
