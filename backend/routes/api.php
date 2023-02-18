6a726c2fb054d7a98c6bf1ed4c569865118748c9
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-02-18 17:59:04 +0700

    Load job model.

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -27,7 +27,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
             $user->load('employee');
             break;
         case 'society':
-            $user->load('society');
+            $user->load('society', 'society.job');
             break;
     }
 
