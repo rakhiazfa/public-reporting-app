d0ca75007b461c838c4027f0ffa66a5ebed0702d
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-02-19 02:50:22 +0700

    Implementing create agency method.

--- backend/resources/views/agency.blade.php
+++ backend/resources/views/agency.blade.php
@@ -1,5 +1,11 @@
 <x-cube.layout title="Daftar Instansi">
 
+    @if (session('success'))
+        <div class="bg-emerald-400 rounded-lg px-5 py-3 mb-7">
+            <p class="text-gray-50 font-normal">{{ session('success') }}</p>
+        </div>
+    @endif
+
     <section class="mb-7">
 
         <div class="grid grid-cols-1">
