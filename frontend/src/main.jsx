8594599273702c6d3095acdb120b555c983a8acc
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-02-22 05:32:01 +0700

    Initialize frontend.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import axios from "axios";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./styles/globals.css";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+/**
+ * Setup axios.
+ *
+ */
+
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND_API;
+
+/**
+ * Render application.
+ *
+ */
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
+);
