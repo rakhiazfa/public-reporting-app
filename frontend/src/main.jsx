40200de3c8f50bb8b66699ee10ae05f34ccf13ef
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-02-22 05:14:27 +0700

    Redesign frontend.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,25 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./styles/globals.css";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import App from "./App";
-import axios from "axios";
-import store from "./store";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
 
-/**
- * Setup axios.
- *
- */
-
-axios.defaults.baseURL = import.meta.env.VITE_BACKEND_API;
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <Provider store={store}>
-                <App />
-            </Provider>
-        </BrowserRouter>
-    </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        port: 3000,
-    },
-    build: { chunkSizeWarningLimit: 1600 },
-});
+  plugins: [react()],
+})
