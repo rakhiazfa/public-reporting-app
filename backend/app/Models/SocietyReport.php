079ca1dcce1f81627ab44f6799e51998fe617574
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-02-23 02:33:40 +0700

    Initialize society report model and migration.

--- backend/app/Models/SocietyReport.php
+++ backend/app/Models/SocietyReport.php
@@ -4,8 +4,42 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class SocietyReport extends Model
 {
     use HasFactory;
+
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'ticket_id', 'image', 'title', 'body', 'data',
+        'status', 'attachment', 'report_subcategories_id',
+        'agency_id',
+    ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function category()
+    {
+        return $this->belongsTo(ReportSubcategory::class, 'report_subcategories_id', 'id');
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function destination()
+    {
+        return $this->belongsTo(Agency::class, 'agency_id', 'id');
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function author()
+    {
+        return $this->belongsTo(Society::class, 'society_id', 'id');
+    }
 }
--- backend/database/migrations/2023_02_22_190527_create_society_reports_table.php
+++ backend/database/migrations/2023_02_22_190527_create_society_reports_table.php
@@ -15,6 +15,23 @@ return new class extends Migration
     {
         Schema::create('society_reports', function (Blueprint $table) {
             $table->id();
+
+            $table->string('ticket_id')->unique();
+
+            $table->enum('type', ['pengaduan', 'aspirasi', 'permintaan-informasi']);
+            $table->string('image')->nullable();
+            $table->string('title');
+            $table->text('body');
+            $table->date('date');
+            $table->enum('status', ['process', 'accepted', 'rejected']);
+            $table->string('attachment');
+
+            $table->foreignId('report_subcategories_id')->constrained('report_subcategories');
+
+            $table->foreignId('agency_id')->constrained('agencies');
+
+            $table->foreignId('society_id')->constrained('societies');
+
             $table->timestamps();
         });
     }
