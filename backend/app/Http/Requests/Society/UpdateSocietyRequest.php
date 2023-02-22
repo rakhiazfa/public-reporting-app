43d398b207f5fa7399144424c2881d1564332d65
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-02-23 01:19:27 +0700

    Update request - update society request.

--- backend/app/Http/Requests/Society/UpdateSocietyRequest.php
+++ backend/app/Http/Requests/Society/UpdateSocietyRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests\Society;
 
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rule;
 
 class UpdateSocietyRequest extends FormRequest
@@ -27,7 +28,7 @@ class UpdateSocietyRequest extends FormRequest
     {
         $society = $this->route('society');
 
-        return [
+        $rules = [
             'nik' => ['required', 'unique:societies,nik,' . $society->id],
             'name' => ['required'],
             'date_of_birth' => ['required', 'date'],
@@ -36,12 +37,29 @@ class UpdateSocietyRequest extends FormRequest
             'job_id' => ['required'],
             'email' => ['required', 'unique:users,email,' . $society->user->id],
             'username' => ['required', 'unique:users,username,' . $society->user->id, 'without_spaces'],
-            'password' => ['required', 'min:8', 'confirmed'],
             'country' => ['nullable'],
             'province' => ['required'],
             'city' => ['required'],
             'postal_code' => ['required'],
             'address' => ['required'],
         ];
+
+        $this->input('password', false) && $rules['password'] = ['required', 'min:8', 'confirmed'];
+
+        return $rules;
+    }
+
+    /**
+     * @return void
+     */
+    protected function passedValidation()
+    {
+        $society = $this->route('society');
+
+        $password = $this->input('password', false);
+
+        $this->merge([
+            'password' => $password ? Hash::make($password) : $society->user->password,
+        ]);
     }
 }
