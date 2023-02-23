4d2be0be99d9041d2705f7deb658d6f53dc62808
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-02-23 11:26:58 +0700

    Initialize society report request.

---
+++ backend/app/Http/Requests/SocietyReport/StoreSocietyReportRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests\SocietyReport;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
+
+class StoreSocietyReportRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return Auth::check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'type' => ['required', Rule::in(['pengaduan', 'aspirasi', 'permintaan-informasi'])],
+            'image' => ['nullable'],
+            'title' => ['required'],
+            'body' => ['required'],
+            'date' => ['required', 'date'],
+            'attachment' => ['required'],
+            'report_subcategory_id' => ['required'],
+            'agency_id' => ['required'],
+        ];
+    }
+}
