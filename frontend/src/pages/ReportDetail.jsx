db460fa01cdb7226903ebd25e3f14455ef8c3bf7
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-02-23 15:35:49 +0700

    Create report details page.

--- frontend/src/pages/ReportDetail.jsx
+++ frontend/src/pages/ReportDetail.jsx
@@ -25,7 +25,7 @@ export default function ReportDetail() {
         <Layout title={`${report?.title} - Lapmas`}>
             <section>
                 <div className="wrapper">
-                    <div className="flex items-center gap-5 mb-7">
+                    <div className="flex items-center gap-5 mb-16">
                         <div className="w-[45px] h-[45px] bg-gray-400 rounded-full"></div>
                         <div>
                             <span className="block mb-1">
@@ -38,18 +38,21 @@ export default function ReportDetail() {
                         <h1 className="text-xl lg:text-3xl font-bold mb-7">
                             {report?.title}
                         </h1>
-                        <p className="mb-5">{nl2br(report?.body)}</p>
-                        <Link
-                            className="text-blue-500 hover:underline"
-                            target="_blank"
-                            to={
-                                import.meta.env.VITE_BACKEND_URL +
-                                "/storage/" +
-                                report?.attachment
-                            }
-                        >
-                            Lampiran
-                        </Link>
+                        <p className="mb-10">{nl2br(report?.body)}</p>
+                        <div className="flex items-center gap-20">
+                            <p className="text-gray-400">{report?.ticket_id}</p>
+                            <Link
+                                className="text-blue-500 hover:underline"
+                                target="_blank"
+                                to={
+                                    import.meta.env.VITE_BACKEND_URL +
+                                    "/storage/" +
+                                    report?.attachment
+                                }
+                            >
+                                Lampiran
+                            </Link>
+                        </div>
                     </div>
                 </div>
             </section>
