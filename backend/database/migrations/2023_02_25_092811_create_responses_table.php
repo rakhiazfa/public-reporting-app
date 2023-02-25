ed43af8d59a57a3ed12066daa4a2eca98854611d
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-02-25 18:06:29 +0700

    Create response model.

---
+++ backend/database/migrations/2023_02_25_092811_create_responses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('responses', function (Blueprint $table) {
+            $table->id();
+            $table->text('response');
+
+            $table->foreignId('society_report_id')->constrained('society_reports');
+
+            $table->foreignId('agency_id')->constrained('agencies');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('responses');
+    }
+};
