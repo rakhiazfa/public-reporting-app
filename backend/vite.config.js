5a6ccd324139dc9b94824974b28538b0f1874fca
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-02-27 14:29:12 +0700

    Update chunk size warning limit.

--- backend/vite.config.js
+++ backend/vite.config.js
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/cube.css', 'resources/js/cube.js'],
+            input: ["resources/css/cube.css", "resources/js/cube.js"],
             refresh: true,
         }),
     ],
+    build: {
+        chunkSizeWarningLimit: 1600,
+    },
 });
