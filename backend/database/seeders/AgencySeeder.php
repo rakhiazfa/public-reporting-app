2a93cef7e2f8fbad5133507c1ba45c50a43f28ee
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-03-13 17:00:13 +0700

    Update agency seeder.

--- backend/database/seeders/AgencySeeder.php
+++ backend/database/seeders/AgencySeeder.php
@@ -17,6 +17,114 @@ class AgencySeeder extends Seeder
      */
     public function run()
     {
+        // 
+
+        $agency = Agency::create([
+            'name' => 'Presiden dan Wakil Presiden',
+            'user_id' => User::factory()->state([
+                'name' => 'Presiden dan Wakil Presiden',
+                'role_id' => 2
+            ])->create()->id,
+        ]);
+
+        AgencyLocation::factory()->state(['agency_id' => $agency->id])->create();
+
+        // 
+
+        $agency = Agency::create([
+            'name' => 'Dewan Perwakilan Rakyat',
+            'user_id' => User::factory()->state([
+                'name' => 'Dewan Perwakilan Rakyat',
+                'role_id' => 2
+            ])->create()->id,
+        ]);
+
+        AgencyLocation::factory()->state(['agency_id' => $agency->id])->create();
+
+        // 
+
+        $agency = Agency::create([
+            'name' => 'Majelis Perwakilan Rakyat',
+            'user_id' => User::factory()->state([
+                'name' => 'Majelis Perwakilan Rakyat',
+                'role_id' => 2
+            ])->create()->id,
+        ]);
+
+        AgencyLocation::factory()->state(['agency_id' => $agency->id])->create();
+
+        // 
+
+        $agency = Agency::create([
+            'name' => 'Mahkamah Konstitusi',
+            'user_id' => User::factory()->state([
+                'name' => 'Mahkamah Konstitusi',
+                'role_id' => 2
+            ])->create()->id,
+        ]);
+
+        AgencyLocation::factory()->state(['agency_id' => $agency->id])->create();
+
+        // 
+
+        $agency = Agency::create([
+            'name' => 'Mahkamah Agung',
+            'user_id' => User::factory()->state([
+                'name' => 'Mahkamah Agung',
+                'role_id' => 2
+            ])->create()->id,
+        ]);
+
+        AgencyLocation::factory()->state(['agency_id' => $agency->id])->create();
+
+        // 
+
+        $agency = Agency::create([
+            'name' => 'Badan Pemeriksa Keuangan',
+            'user_id' => User::factory()->state([
+                'name' => 'Badan Pemeriksa Keuangan',
+                'role_id' => 2
+            ])->create()->id,
+        ]);
+
+        AgencyLocation::factory()->state(['agency_id' => $agency->id])->create();
+
+        // 
+
+        $agency = Agency::create([
+            'name' => 'Pemerintahan Kota Tegal',
+            'user_id' => User::factory()->state([
+                'name' => 'Pemerintahan Kota Tegal',
+                'role_id' => 2
+            ])->create()->id,
+        ]);
+
+        AgencyLocation::factory()->state(['agency_id' => $agency->id])->create();
+
+        // 
+
+        $agency = Agency::create([
+            'name' => 'Pemerintahan Kota Semarang',
+            'user_id' => User::factory()->state([
+                'name' => 'Pemerintahan Kota Semarang',
+                'role_id' => 2
+            ])->create()->id,
+        ]);
+
+        AgencyLocation::factory()->state(['agency_id' => $agency->id])->create();
+
+        // 
+
+        $agency = Agency::create([
+            'name' => 'Pemerintahan DKI Jakarta',
+            'user_id' => User::factory()->state([
+                'name' => 'Pemerintahan DKI Jakarta',
+                'role_id' => 2
+            ])->create()->id,
+        ]);
+
+        AgencyLocation::factory()->state(['agency_id' => $agency->id])->create();
+
         //
 
         $agency = Agency::create([
@@ -29,20 +137,72 @@ class AgencySeeder extends Seeder
 
         AgencyLocation::factory()->state(['agency_id' => $agency->id])->create();
 
+        // 
+
         $agency = Agency::create([
-            'name' => 'Pemerintahan Kota Semarang',
+            'name' => 'Pemerintahan Kab Sumedang',
             'user_id' => User::factory()->state([
-                'name' => 'Pemerintahan Kota Semarang',
+                'name' => 'Pemerintahan Kab Sumedang',
                 'role_id' => 2
             ])->create()->id,
         ]);
 
         AgencyLocation::factory()->state(['agency_id' => $agency->id])->create();
 
+        // 
+
         $agency = Agency::create([
-            'name' => 'Pemerintahan Kota Tegal',
+            'name' => 'Pemerintahan Kab Cimahi',
             'user_id' => User::factory()->state([
-                'name' => 'Pemerintahan Kota Tegal',
+                'name' => 'Pemerintahan Kab Cimahi',
+                'role_id' => 2
+            ])->create()->id,
+        ]);
+
+        AgencyLocation::factory()->state(['agency_id' => $agency->id])->create();
+
+        // 
+
+        $agency = Agency::create([
+            'name' => 'Pemerintahan Provinsi Jawa Barat',
+            'user_id' => User::factory()->state([
+                'name' => 'Pemerintahan Provinsi Jawa Barat',
+                'role_id' => 2
+            ])->create()->id,
+        ]);
+
+        AgencyLocation::factory()->state(['agency_id' => $agency->id])->create();
+
+        // 
+
+        $agency = Agency::create([
+            'name' => 'Pemerintahan Kota Pontianak',
+            'user_id' => User::factory()->state([
+                'name' => 'Pemerintahan Kota Pontianak',
+                'role_id' => 2
+            ])->create()->id,
+        ]);
+
+        AgencyLocation::factory()->state(['agency_id' => $agency->id])->create();
+
+        // 
+
+        $agency = Agency::create([
+            'name' => 'Pemerintahan Kota Bekasi',
+            'user_id' => User::factory()->state([
+                'name' => 'Pemerintahan Kota Bekasi',
+                'role_id' => 2
+            ])->create()->id,
+        ]);
+
+        AgencyLocation::factory()->state(['agency_id' => $agency->id])->create();
+
+        // 
+
+        $agency = Agency::create([
+            'name' => 'Pemerintahan Kota Bogor',
+            'user_id' => User::factory()->state([
+                'name' => 'Pemerintahan Kota Bogor',
                 'role_id' => 2
             ])->create()->id,
         ]);
