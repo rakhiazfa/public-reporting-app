0206abd6d9d2843a6d7e14dc838f69cc8e89d959
Fauzan Achmad <nmeylani49@gmail.com>
2023-03-15 10:35:22 +0700

    Update

--- backend/resources/views/dashboard.blade.php
+++ backend/resources/views/dashboard.blade.php
@@ -5,31 +5,31 @@
         <div class="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-7">
 
             @if (auth()->user()->hasRole('admin'))
-                <x-cube.card title="Total Instansi">
-                    <div class="flex items-center gap-5">
-                        <i class="uil uil-building text-4xl text-blue-500"></i>
-                        <h3 class="text-xl font-semibold">{{ $agencyCount }}</h3>
-                    </div>
-                </x-cube.card>
+            <x-cube.card title="Total Instansi">
+                <div class="flex items-center gap-5">
+                    <i class="uil uil-building text-4xl text-blue-500"></i>
+                    <h3 class="text-xl font-semibold">30</h3>
+                </div>
+            </x-cube.card>
             @endif
 
             @if (auth()->user()->hasRole('admin') ||
-                    auth()->user()->hasRole('agency'))
-                <x-cube.card title="Total Petugas">
-                    <div class="flex items-center gap-5">
-                        <i class="uil uil-user-md text-4xl text-blue-500"></i>
-                        <h3 class="text-xl font-semibold">{{ $employeeCount }}</h3>
-                    </div>
-                </x-cube.card>
+            auth()->user()->hasRole('agency'))
+            <x-cube.card title="Total Petugas">
+                <div class="flex items-center gap-5">
+                    <i class="uil uil-user-md text-4xl text-blue-500"></i>
+                    <h3 class="text-xl font-semibold">{{ $employeeCount }}</h3>
+                </div>
+            </x-cube.card>
             @endif
 
             @if (auth()->user()->hasRole('admin'))
-                <x-cube.card title="Total Masyarakat">
-                    <div class="flex items-center gap-5">
-                        <i class="uil uil-user text-4xl text-blue-500"></i>
-                        <h3 class="text-xl font-semibold">{{ $societyCount }}</h3>
-                    </div>
-                </x-cube.card>
+            <x-cube.card title="Total Masyarakat">
+                <div class="flex items-center gap-5">
+                    <i class="uil uil-user text-4xl text-blue-500"></i>
+                    <h3 class="text-xl font-semibold">{{ $societyCount }}</h3>
+                </div>
+            </x-cube.card>
             @endif
 
             <x-cube.card title="Total Pengaduan">
