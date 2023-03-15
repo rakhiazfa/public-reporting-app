879b1062c8a9fadb20f2356b883767f46fca468d
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-03-15 22:59:50 +0700

    Fix X-Frame bug.

--- backend/app/Http/Kernel.php
+++ backend/app/Http/Kernel.php
@@ -21,7 +21,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \Illuminate\Http\Middleware\FrameGuard::class,
+        \App\Http\Middleware\FrameOptionsMiddleware::class,
     ];
 
     /**
@@ -43,7 +43,7 @@ class Kernel extends HttpKernel
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \Illuminate\Http\Middleware\FrameGuard::class,
+            \App\Http\Middleware\FrameOptionsMiddleware::class,
         ],
     ];
 
